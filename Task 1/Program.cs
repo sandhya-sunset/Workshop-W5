e983ab34ebd11a9f07584e9d0d0b7686b0931f8e
sandhya-sunset <sandhyamajhi77@gmail.com>
2025-11-25 11:29:15 +0545

    Task 1

---
+++ Task 1/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+       
+        BankAccount account = new BankAccount("ACCOUNT123", 4000);
+
+       
+        Console.WriteLine("Account Number: " + account.AccountNumber);
+
+        
+        account.Deposit(2000);
+
+        
+        account.Withdraw(1500);
+
+       
+        Console.WriteLine("Remaining Balance: " + account.Balance);
+    }
+}
---
+++ Task 1/bankaccount.cs
@@ -0,0 +1,67 @@
+﻿public class BankAccount
+{
+   
+    private string accountNumber;
+    private double balance;
+
+   
+    public string AccountNumber
+    {
+        get { return accountNumber; }
+    }
+
+    
+    public double Balance
+    {
+        get { return balance; }
+        private set
+        {
+            if (value <= 0)
+            {
+                Console.WriteLine("Balance cannot be zero or negative.");
+            }
+            else
+            {
+                balance = value;
+            }
+        }
+    }
+
+    public BankAccount(string accNum, double openingBalance)
+    {
+        accountNumber = accNum;
+        Balance = openingBalance; 
+    }
+
+   
+    public void Deposit(double amount)
+    {
+        if (amount <= 0)
+        {
+            Console.WriteLine("Deposit amount must be greater than 0.");
+        }
+        else
+        {
+            balance += amount;
+            Console.WriteLine($"Deposited: {amount}");
+        }
+    }
+
+    
+    public void Withdraw(double amount)
+    {
+        if (amount <= 0)
+        {
+            Console.WriteLine("Withdraw amount must be greater than 0.");
+        }
+        else if (amount > balance)
+        {
+            Console.WriteLine("Insufficient balance!");
+        }
+        else
+        {
+            balance -= amount;
+            Console.WriteLine($"Withdrawn: {amount}");
+        }
+    }
+}
