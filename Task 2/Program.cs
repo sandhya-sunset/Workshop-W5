7dcf3717e51b94bbaec2993fb12b8e1720f83cea
sandhya-sunset <sandhyamajhi77@gmail.com>
2025-11-25 11:35:12 +0545

    Task 2

---
+++ Task 2/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+       
+        Car car = new Car();
+        car.Brand = "BMW";
+        car.Speed = 120;
+        car.Seats = 5;
+
+        
+        Motorcycle bike = new Motorcycle();
+        bike.Brand = "Yamaha";
+        bike.Speed = 90;
+        bike.HasCarrier = true;
+
+        
+        car.Start();
+        car.DisplayInfo();
+        car.Stop();
+
+        Console.WriteLine();
+
+        bike.Start();
+        bike.DisplayInfo();
+        bike.Stop();
+    }
+}
---
+++ Task 2/vehicle.cs
@@ -0,0 +1,44 @@
+﻿
+public class Vehicle
+{
+    public string Brand { get; set; }
+    public int Speed { get; set; }
+
+    public void Start()
+    {
+        Console.WriteLine($"{Brand} is starting...");
+    }
+
+    public void Stop()
+    {
+        Console.WriteLine($"{Brand} is stopping...");
+    }
+
+   
+    public virtual void DisplayInfo()
+    {
+        Console.WriteLine($"Brand: {Brand}, Speed: {Speed} km/h");
+    }
+}
+
+
+public class Car : Vehicle
+{
+    public int Seats { get; set; }  
+
+    public override void DisplayInfo()
+    {
+        Console.WriteLine($"Brand: {Brand}, Speed: {Speed} km/h, Seats: {Seats}");
+    }
+}
+
+
+public class Motorcycle : Vehicle
+{
+    public bool HasCarrier { get; set; }  
+
+    public override void DisplayInfo()
+    {
+        Console.WriteLine($"Brand: {Brand}, Speed: {Speed} km/h, Carrier: {HasCarrier}");
+    }
+}
